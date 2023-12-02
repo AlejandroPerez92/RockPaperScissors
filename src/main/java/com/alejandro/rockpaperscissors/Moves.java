45de250007ed3fa364ffeabbd7ff973d699b66c4
Alejandro Perez <alejandro.perez@deliverea.com>
2023-12-02 13:32:07 +0100

    refactor: encapsulate moves beats logic into Moves class

--- src/main/java/com/alejandro/rockpaperscissors/Moves.java
+++ src/main/java/com/alejandro/rockpaperscissors/Moves.java
@@ -1,7 +1,19 @@
 package com.alejandro.rockpaperscissors;
 
+import java.util.HashMap;
+import java.util.List;
+
 public enum Moves {
     ROCK,
     PAPER,
-    SCISSORS,
+    SCISSORS;
+
+    public boolean isBeatenBy(Moves move) {
+        var beats = new HashMap<Moves, List<Moves>>();
+        beats.put(Moves.ROCK, List.of(Moves.PAPER));
+        beats.put(Moves.PAPER, List.of(Moves.SCISSORS));
+        beats.put(Moves.SCISSORS, List.of(Moves.ROCK));
+
+        return beats.get(this).contains(move);
+    }
 }
--- src/main/java/com/alejandro/rockpaperscissors/RockPaperScissors.java
+++ src/main/java/com/alejandro/rockpaperscissors/RockPaperScissors.java
@@ -1,20 +1,13 @@
 package com.alejandro.rockpaperscissors;
 
-import java.util.HashMap;
-import java.util.List;
-
 public class RockPaperScissors {
     public String rockPaperScissors(Moves movePlayer1, Moves movePlayer2) {
-        var beats = new HashMap<Moves, List<Moves>>();
-        beats.put(Moves.ROCK, List.of(Moves.PAPER));
-        beats.put(Moves.PAPER, List.of(Moves.SCISSORS));
-        beats.put(Moves.SCISSORS, List.of(Moves.ROCK));
 
         if (movePlayer1.equals(movePlayer2)) {
             return "draw";
         }
 
-        if (beats.get(movePlayer1).contains(movePlayer2)) {
+        if (movePlayer1.isBeatenBy(movePlayer2)) {
             return "player2";
         }
 
