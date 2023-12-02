c9ef71d3e1e24f70d90b5c06f749197b818f791b
Alejandro Perez <alejandro.perez@deliverea.com>
2023-12-02 13:26:56 +0100

    refactor: transform if logic in array

--- src/main/java/com/alejandro/rockpaperscissors/RockPaperScissors.java
+++ src/main/java/com/alejandro/rockpaperscissors/RockPaperScissors.java
@@ -1,23 +1,23 @@
 package com.alejandro.rockpaperscissors;
 
+import java.util.HashMap;
+import java.util.List;
+
 public class RockPaperScissors {
     public String rockPaperScissors(Moves movePlayer1, Moves movePlayer2) {
+        var beats = new HashMap<Moves, List<Moves>>();
+        beats.put(Moves.ROCK, List.of(Moves.PAPER));
+        beats.put(Moves.PAPER, List.of(Moves.SCISSORS));
+        beats.put(Moves.SCISSORS, List.of(Moves.ROCK));
+
         if (movePlayer1.equals(movePlayer2)) {
             return "draw";
         }
 
-        if (movePlayer1.equals(Moves.ROCK) && movePlayer2.equals(Moves.SCISSORS)) {
-            return "player1";
+        if (beats.get(movePlayer1).contains(movePlayer2)) {
+            return "player2";
         }
 
-        if (movePlayer1.equals(Moves.PAPER) && movePlayer2.equals(Moves.ROCK)) {
-            return "player1";
-        }
-
-        if (movePlayer1.equals(Moves.SCISSORS) && movePlayer2.equals(Moves.PAPER)) {
-            return "player1";
-        }
-
-        return "player2";
+        return "player1";
     }
 }
